04c2112011da19255c1b26a52925c14a163b3dd5
sontt <tson20022003@gmail.com>
2023-12-14 23:57:02 +0700

    btth03

--- BTTH03_2/index.php
+++ BTTH03_2/index.php
@@ -1,6 +1,17 @@
 <?php
-include "header.php";
+require_once 'config.php';
+$controller = isset($_GET["controller"]) ? $_GET["controller"] : 'home';
+$action = isset($_GET["action"]) ? $_GET["action"] : 'index';
 
+$controllerClass = ucfirst($controller) . "Controller";
 
+$controllerFile = "/controller/$controllerClasss.php";
 
-include "footer.php";
+if(file_exists($controllerFile)){
+    require_once $controllerFile;
+    $controllerInstance  = new $controllerClass();
+    $controllerInstance->$action();
+}else {
+    echo "Controller not found";
+}
+?>
