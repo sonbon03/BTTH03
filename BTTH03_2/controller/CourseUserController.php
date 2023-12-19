b4be7a892138dc40ff53bda7cf339009ec3a97ea
Th4nhhh <152381346+Th4nhhh@users.noreply.github.com>
2023-12-19 23:51:13 +0700

    Update CourseUserController.php

--- BTTH03_2/controller/CourseUserController.php
+++ BTTH03_2/controller/CourseUserController.php
@@ -1,59 +1,76 @@
 <?php
 
-class CourseUserController
+class LessonController
 {
-    private $users;
-    private $courses;
+    private $db; 
 
-    public function __construct()
+    public function __construct($db)
     {
-        $this->users = array();
-        $this->courses = array();
+        $this->db = $db;
     }
 
-    public function addUser(User $user)
+    public function createLesson($courseId, $title, $description)
     {
-        $this->users[$user->getId()] = $user;
+        $createdAt = date('Y-m-d H:i:s');
+        $updatedAt = $createdAt;
+
+
+        $query = "INSERT INTO lessons (course_id, title, description, created_at, updated_at) VALUES (?, ?, ?, ?, ?)";
+        $statement = $this->db->prepare($query);
+        $statement->bind_param("issss", $courseId, $title, $description, $createdAt, $updatedAt);
+        $statement->execute();
+
+
+        return $statement->insert_id;
     }
 
-    public function addCourse(Course $course)
+    public function updateLesson($lessonId, $title, $description)
     {
-        $this->courses[$course->getId()] = $course;
+        $updatedAt = date('Y-m-d H:i:s');
+
+
+        $query = "UPDATE lessons SET title = ?, description = ?, updated_at = ? WHERE id = ?";
+        $statement = $this->db->prepare($query);
+        $statement->bind_param("sssi", $title, $description, $updatedAt, $lessonId);
+        $statement->execute();
+
+
+        return $statement->affected_rows > 0;
     }
 
-    public function enrollUserInCourse($userId, $courseId)
+    public function deleteLesson($lessonId)
     {
-        if (!isset($this->users[$userId]) || !isset($this->courses[$courseId])) {
-            return false;
+
+        $query = "DELETE FROM lessons WHERE id = ?";
+        $statement = $this->db->prepare($query);
+        $statement->bind_param("i", $lessonId);
+        $statement->execute();
+
+
+        return $statement->affected_rows > 0;
+    }
+
+    public function getLesson($lessonId)
+    {
+        $query = "SELECT * FROM lessons WHERE id = ?";
+        $statement = $this->db->prepare($query);
+        $statement->bind_param("i", $lessonId);
+        $statement->execute();
+        $result = $statement->get_result();
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            return new Lesson(
+                $row['id'],
+                $row['course_id'],
+                $row['title'],
+                $row['description'],
+                $row['created_at'],
+                $row['updated_at']
+            );
+        } else {
+            return null;
         }
-
-        $user = $this->users[$userId];
-        $course = $this->courses[$courseId];
-
-        $user->addCourse($course);
-        $course->addUser($user);
-
-        return true;
-    }
-
-    public function getUserCourses($userId)
-    {
-        if (!isset($this->users[$userId])) {
-            return array();
-        }
-
-        $user = $this->users[$userId];
-        return $user->getCourses();
-    }
-
-    public function getCourseUsers($courseId)
-    {
-        if (!isset($this->courses[$courseId])) {
-            return array();
-        }
-
-        $course = $this->courses[$courseId];
-        return $course->getUsers();
     }
 }
 
