40012d9b8309600b22107ea1968cc2eabd6a7f75
quydeptrailangtu <152381539+quydeptrailangtu@users.noreply.github.com>
2023-12-19 23:47:19 +0700

    Update QuizzesController.php

---
+++ BTTH03_2/controller/QuizzesController.php
@@ -0,0 +1,37 @@
+
+<?php
+// require_once "../models/Quizzes.php";
+include "../service/QuizzesService.php";
+
+class QuizzesController{
+        public function index(){
+            $quizzesService = new QuizzesService();
+            $quizzes = $quizzesService::getAll();
+            require 'views/quizzes/index.php';
+        }
+
+        public function  create(){
+            require 'views/quizzes/create.php';
+        }
+
+        public function store(){
+            $id = $_POST['id'];
+            $lesson_id = $_POST['lesson_id'];
+            $title = $_POST['title'];
+            $created_at = $_POST['created_at'];
+            $updated_at = $_POST['updated_at'];
+            $questions = new Quizzes();
+            $questions->setId($id);
+            $questions->setLesson_id($lesson_id);
+            $questions->setTitle($title);
+            $questions->setCreatedAt($created_at);
+            $questions->setUpdatedAt($updated_at);
+
+            header('Location: index.php?controller=quizzes&action=index');
+        }
+        public function edit(){
+            $id = $_GET['id'];
+            $questions = Quizzes::getIdData();
+            require '../views/quizzes/edit.php';
+        }
+    }
